bfb2293dd80914e6fee763572911a529f1d7cc37
Sam Ashurst <95435776+SamAshurst@users.noreply.github.com>
2022-02-24 13:00:42 +0000

    Simple structure idea

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      <h2>App</h2>
+      <h3>Header</h3>
+      <table>
+        <tr>
+          <th>Tasklist</th>
+        </tr>
+        <tr>
+          <td>Task Complete </td>
+          <td>Task </td>
+          <td>Category </td>
+          <td>To do by date </td>
+          <td>Days left</td>
+        </tr>
+        <tr>
+          <td>Checkbox</td>
+          <td>Button user input text</td>
+          <td>Drop down list</td>
+          <td>Input type date</td>
+          <td>Function remaining days</td>
+        </tr>
+      </table>
+    </div>
+  );
 }
 
 export default App;
